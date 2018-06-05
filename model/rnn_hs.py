bac3b5396a2c182017dd0e33578180833d88e5d7
liangoy <emailboxbeta@gmail.com>
2018-06-05 08:45:23 +0800

    20180605

--- model/rnn_hs.py
+++ model/rnn_hs.py
@@ -27,11 +27,11 @@ dopen_hs = data_t[:, 6] / data_t_1[:, 9]
 dhigh_hs = data_t[:, 7] / data_t_1[:, 9]
 dlow_hs = data_t[:, 8] / data_t_1[:, 9]
 dclose_hs = data_t[:, 9] / data_t_1[:, 9]
-# dvolume_hs = data_t[:, 11] / data_t_1[:, 11]
+dvolume_hs = data_t[:, 11] / data_t_1[:, 11]
 
-data = np.concatenate([dopen, dhigh, dlow, dclose, dvolume, dopen_hs, dhigh_hs, dlow_hs, dclose_hs],
+data = np.concatenate([dopen, dhigh, dlow, dclose, dvolume, dopen_hs, dhigh_hs, dlow_hs, dclose_hs, dvolume_hs],
                       axis=0) - 1
-data = np.reshape(data, [-1, 9], order='F')
+data = np.reshape(data, [-1, 10], order='F')
 
 data_train = data[:-1 * batch_size - long + 1]
 data_test = data[-1 * batch_size - long + 1:]
@@ -47,13 +47,13 @@ def next(data, bs=batch_size, random=True):
     for i in r:
         sample = data[i: i + long]
         a.append(sample[:-1, :5])
-        b.append(sample[:, 5:9])
-        c.append(sample[-1][2])
+        b.append(sample[:-1, 5:10])
+        c.append(sample[-1][3])
     return a, b, c
 
 
-x = tf.placeholder(shape=[batch_size, long -1, 5], dtype=tf.float32)
-y = tf.placeholder(shape=[batch_size, long, 4], dtype=tf.float32)
+x = tf.placeholder(shape=[batch_size, long - 1, 5], dtype=tf.float32)
+y = tf.placeholder(shape=[batch_size, long - 1, 5], dtype=tf.float32)
 z_ = tf.placeholder(shape=[batch_size], dtype=tf.float32)
 
 X = tf.nn.sigmoid(x) - 0.5
@@ -71,7 +71,7 @@ with tf.variable_scope('RNN_x'):
 gru_y = GRUCell(num_units=8, reuse=tf.AUTO_REUSE, activation=tf.nn.elu)
 state_y = gru_y.zero_state(batch_size, dtype=tf.float32)
 with tf.variable_scope('RNN_y'):
-    for timestep in range(long-1):#be careful
+    for timestep in range(long - 1):  # be careful
         if timestep == 1:
             tf.get_variable_scope().reuse_variables()
         (cell_output_y, state_y) = gru_y(Y[:, timestep], state_y)
